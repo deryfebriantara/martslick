94618b04d049c39d42b94e9a2997d7860013ec24
dery <deryfebriantara@gmail.com>
2016-04-02 14:29:10 +0700

    config

--- application/config/database.php
+++ application/config/database.php
@@ -49,7 +49,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'dingdungfr4t3rnal';
+$db['default']['password'] = '123123password';
 $db['default']['database'] = 'martspulsa';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
